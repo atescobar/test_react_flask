e9f351311af285d733369c8dd20b817ccbd6f16f
Agustin Escobar <agustin.escobar.blanc@gmail.com>
2020-05-30 11:44:36 -0400

    test_react_flask

---
+++ api/api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import time 
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+    return {
+        'time': time.time()
+    }
+
+if __name__ == "__main__":
+    app.run()
